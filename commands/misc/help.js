1c00a013cb7d61c1304c6e35cfe225bb4387e9ad
adrifcastr <adrifcastr@gmail.com>
2020-05-22 18:15:01 +0200

    make help tags work

--- commands/misc/help.js
+++ commands/misc/help.js
@@ -19,7 +19,6 @@ class Help extends Command {
      * @param {Akairo.Argument} args 
      */
     async exec(message, args) {
-        console.log(args.modules);
         const fsurl = 'https://discordapp.com/channels/595318490240385037/595935089070833708';
         const customprefix = this.client.getGuild.get(message.guild.id);
         const prefixes = `\`${customprefix.prefix}\` | ` + Util.GetPrefix(message).filter(x => x != customprefix.prefix).map(x => `\`${x}\``).join(' | ') + ` <@${this.client.user.id}>`;
@@ -45,6 +44,27 @@ class Help extends Command {
         if (args.modules.match(/(?:syntax)/i)) {
             const help = Util.CreateEmbed('__Command Syntax:__', null, message.member)
                 .setDescription('Gideon\'s prefixes are: ' + prefixes + '\nArguments wrapped in `<>` are variables. _do not actually add brackets_\nArguments seperated by `/` mean `this or(/) this`.\nArguments wrapped in `[]` are optional arguments.\nCommands marked with :warning: are potentially dangerous.\nCommands marked with <:18:693135780796694668> are potentially NSFW.\nCommands marked with <:timevault:686676561298063361> are Time Vault only.\nCommands marked with <:gideon:686678560798146577> are application owner only.\nCommands marked with <:voicerecognition:693521621184413777> are Gideon Voice™ compatible.\nCommands marked with <:perms:686681300156940349> require certain permissions.\nCommands marked with `@role` require the mentioned role.')  
+                .addField('Feature Suggestions:', `**[Click here to suggest a feature](${fsurl} 'Time Vault - #feature-suggestions')**`);
+    
+            return message.channel.send(help);
+        }
+
+        if (args.modules.match(/(?:tags)/i)) {
+            let cmds = {};
+            let tags = [];
+            for (let item of commands) {
+                console.log(item.usage);
+                console.log(item.description);
+                let cmd = this.handler.modules.get(item.id);
+    
+                if (cmd.category == 'tags') cmds[item.usage] = item.description;
+            }
+    
+            for (let item in cmds) tags.push(item);
+            const tagnames = tags.map(x => `\`${x}\``).join(' ');
+    
+            const help = Util.CreateEmbed('__Available tags:__ <:timevault:686676561298063361>', null, message.member)
+                .setDescription('Use `' + customprefix.prefix + 'help syntax` for command syntax explanations\nUse `' + customprefix.prefix + 'alias <command>` for command aliases\nUse `' + customprefix.prefix + 'search <command>` to search a command\nGideon\'s prefixes are: ' + prefixes + '\n\n' + tagnames)  
                 .addField('Feature Suggestions:', `**[Click here to suggest a feature](${fsurl} 'Time Vault - #feature-suggestions')**`);
     
             return message.channel.send(help);
